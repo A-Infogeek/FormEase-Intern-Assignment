2de0ead865c3cd2531b97c81eca75026534f6cd4
A-Infogeek <rishabhraj026@gmail.com>
2023-12-08 17:54:05 +0530

    added more fields to logistics section

--- src/component/Logistics/Logistics.jsx
+++ src/component/Logistics/Logistics.jsx
@@ -14,6 +14,64 @@ const Logistics = () => {
                     <p>Upload Cover Page (optional)</p>
                     <div className='fileUpload'><p>Choose file here</p><i class="fa-solid fa-arrow-up-from-bracket"></i></div>
                 </div>
+                <div className="logisticsInput">
+                    {/* first row */}
+                    <div className="name ll">
+                        <div className="box">
+                            <label htmlFor="fname">Style of cause</label>
+                            <input type="text" className='boxInput' placeholder='Type here' />
+                        </div>
+                        <div className="box">
+                            <label >Court File#</label>
+                            <input type="text" className='boxInput' placeholder='Type here' />
+                        </div>
+                        <div className="box">
+                            <label >Booking Date</label>
+                            <select name="fname" id="date" className='boxInput'>
+                                <option ><i class="fa-regular fa-calendar"></i>  -----</option>
+                            </select>
+                        </div>
+                    </div>
+                    {/* second row */}
+                    <div className="name ll">
+                        <div className="box box3">
+                            <label htmlFor="fname">Start Time</label>
+                            <select name="fname" className='boxInput'></select>
+                        </div>
+                        <div className="box box3">
+                            <label >End Time</label>
+                            <select name="fname" className='boxInput'></select>
+                        </div>
+                        <div className="box box3">
+                            <label >Time Zone</label>
+                            <input type="text" className='boxInput' />
+                        </div>
+                    </div>
+                    {/* third row */}
+                    <div className="name ">
+                        <div className="box">
+                            <label >Arbitrator(s)</label>
+                            <input type="text" className='boxInput' placeholder='Name' />
+                        </div>
+                        <div className="dots"><i class="fa-solid fa-ellipsis"></i></div>
+                        <div className="box">
+                            <input type="text" className='email boxInput' placeholder='Email' />
+                        </div>
+                    </div>
+
+                    <div className="name">
+                        <div className="box" id="box2">
+                            <div className="add">
+                                <i class="fa-solid fa-plus"></i>
+                            </div>
+                            <input type="text" className='email boxInput' placeholder='Add additional counsel' />
+                        </div>
+                        <div className="dots"><i class="fa-solid fa-ellipsis"></i></div>
+                        <div className="box">
+                            <input type="text" className='email boxInput' />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
